dea33bf387a8a2344120a15da0615742e7cbab6e
Junior <ntshembohlongwane1@gmail.com>
2022-01-11 19:09:16 +0200

    UPdate: file changes

--- Project.js
+++ Project.js
@@ -3,41 +3,37 @@ const btn = document.getElementById(`btn`);
 const email = document.getElementById(`email`);
 const password = document.getElementById(`password`);
 const url = `https://randomuser.me/api/?results=10`;
-const container = document.getElementById('container');
-const edit = document.getElementById('edit');
-const modal_btn = document.getElementById(`modal-btn`)
+const container = document.getElementById("container");
+const edit = document.getElementById("edit");
+const modal_btn = document.getElementById(`modal-btn`);
 
 const isAdmin = "@schoolofit";
-localStorage.setItem('isAdmin', isAdmin);
 const Admin = localStorage.getItem("isAdmin");
+const myAdmin = localStorage.setItem("lasldas");
 
-
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (email.value === Admin) {
-        fetchdataAdmin();
-        container.style.display = "none";
-    }
-    else {
-        fetchdataUser();
-        container.style.display = "none";
-    }
-})
-
-
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (email.value === Admin) {
+    fetchdataAdmin();
+    container.style.display = "none";
+  } else {
+    fetchdataUser();
+    container.style.display = "none";
+  }
+});
 
 function fetchdataUser() {
-    fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw Error('ERROR');
-        }
-        return response.json();
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
     })
-    .then(data => {
-        const html = data.results
-        .map(user => {
-            return `
+    .then((data) => {
+      const html = data.results
+        .map((user) => {
+          return `
             <tr>
                 <td>${user.name.title}</td>
                 <td>${user.name.first}</td>
@@ -47,26 +43,26 @@ function fetchdataUser() {
             </tr>
             `;
         })
-        .join('');
-        tbody.innerHTML = html;
+        .join("");
+      tbody.innerHTML = html;
     })
-    .catch(error => {
-        console.log(error);
+    .catch((error) => {
+      console.log(error);
     });
 }
 
 function fetchdataAdmin() {
-    fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw Error('ERROR');
-        }
-        return response.json();
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
     })
-    .then(data => {
-        const html = data.results
-        .map(user => {
-            return `
+    .then((data) => {
+      const html = data.results
+        .map((user) => {
+          return `
             <tr id="row">
                 <td>${user.name.title}</td>
                 <td>${user.name.first}</td>
@@ -78,19 +74,12 @@ function fetchdataAdmin() {
             </tr>
             `;
         })
-        .join('');
-        tbody.insertAdjacentHTML('afterbegin',html);
+        .join("");
+      tbody.insertAdjacentHTML("afterbegin", html);
     })
-    .catch(error => {
-        console.log(error);
+    .catch((error) => {
+      console.log(error);
     });
 }
 
-
-
 // MODAL
-
-
-
-
-
